4cdca9526e0acb749302a45b7e73d5ff718b79b1
prabhat0528 <prabhatrai0528@gmail.com>
2025-12-07 11:30:46 +0530

    Changes user schema

--- backend/models/user_model.js
+++ backend/models/user_model.js
@@ -1,12 +1,31 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-  role: String,
-  profile_picture: String,
-
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["Mentor", "Student"], 
+  },
+  profile_picture: {
+    type: String,
+    default: "", 
+  },
   mychats: [
     {
       type: mongoose.Schema.Types.ObjectId,

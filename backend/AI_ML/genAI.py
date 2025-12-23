76bf210f82d4ddeb394a90f94ca1ce1f3de26ecd
prabhat0528 <prabhatrai0528@gmail.com>
2025-12-23 16:07:41 +0530

    Just fixing the genAI backend

--- backend/AI_ML/genAI.py
+++ backend/AI_ML/genAI.py
@@ -183,4 +183,4 @@ def generate_roadmap():
 # RUN
 # ==================================================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
